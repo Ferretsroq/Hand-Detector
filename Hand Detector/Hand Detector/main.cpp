2184a24df79b7e15d5595df3f38151698b12495c
Ferretsroq <Ferretsroq@gmail.com>
2016-04-11 15:58:50 -0400

    Initial Commit
    Base images and configuring the xcode file. Haven’t touched computer
    vision since the summer, so not sure how much progress I’ll
    realistically get to.

---
+++ Hand Detector/Hand Detector/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Hand Detector
+//
+//  Created by seth on 4/11/16.
+//  Copyright © 2016 Seth. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+int main(int argc, const char * argv[])
+{
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
